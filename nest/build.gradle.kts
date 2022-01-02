789ab61a05b5b270d2d1b998edd305c324a8add9
Tompee <tompee@drivemode.com>
2022-01-02 22:35:25 +0100

    Add nest as an android module

---
+++ nest/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("com.diffplug.spotless")
+}
+
+apply("../spotless.gradle")
+
+android {
+    compileSdk = Versions.compileSdk
+
+    defaultConfig {
+        minSdk = Versions.minSdk
+        targetSdk = Versions.targetSdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+}
+
+dependencies {
+    implementation(Libs.Kotlin.stdlib)
+    implementation(Libs.AndroidX.core)
+}
