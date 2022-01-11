544ea7483638f23a3469256a4865cecf34d0b152
Tompee Balauag <tompee26@gmail.com>
2022-01-11 22:51:12 +0100

    Publish dependencies in POM (#21)
    * Add maven local
    
    * Publish components

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ buildscript {
     repositories {
         google()
         mavenCentral()
+        mavenLocal()
     }
     dependencies {
         classpath(Libs.androidPlugin)
@@ -13,7 +14,7 @@ buildscript {
 }
 project.ext {
     set("PUBLISH_GROUP_ID", "io.github.tompee26")
-    set("PUBLISH_VERSION", "0.0.2")
+    set("PUBLISH_VERSION", "0.0.2-1")
     set("PUBLISH_URL", "https://github.com/tompee26/arctic-tern")
     set("PUBLISH_LICENSE_NAME", "MIT License")
     set("PUBLISH_LICENSE_URL", "https://github.com/tompee26/arctic-tern/blob/main/LICENSE.md")
