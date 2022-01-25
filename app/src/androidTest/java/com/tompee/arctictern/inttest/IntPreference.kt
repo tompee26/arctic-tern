a6dc3e7268297d5d6307a56cf787104da64fdbc4
Tompee <tompee@drivemode.com>
2022-01-25 12:30:25 +0900

    Add constructor parameters in preferences

--- app/src/androidTest/java/com/tompee/arctictern/inttest/IntPreference.kt
+++ app/src/androidTest/java/com/tompee/arctictern/inttest/IntPreference.kt
@@ -3,8 +3,8 @@ package com.tompee.arctictern.inttest
 import com.tompee.arctictern.nest.ArcticTern
 
 @ArcticTern(preferenceFile = "pref_int", version = 1)
-abstract class IntPreference {
+abstract class IntPreference(private val provider: Provider<Int>) {
 
     @ArcticTern.Property
-    open var counter = 12
+    open var counter = provider.get()
 }
