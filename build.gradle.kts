d4c6c8060f0de801d8bf27a77f30b497df2f52d0
Tompee <tompee@drivemode.com>
2022-03-02 18:35:51 +0900

    Prepare for Alpha 7 Release

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
 }
 project.ext {
     set("PUBLISH_GROUP_ID", "io.github.tompee26")
-    set("PUBLISH_VERSION", "0.0.6")
+    set("PUBLISH_VERSION", "0.0.7")
     set("PUBLISH_URL", "https://github.com/tompee26/arctic-tern")
     set("PUBLISH_LICENSE_NAME", "MIT License")
     set("PUBLISH_LICENSE_URL", "https://github.com/tompee26/arctic-tern/blob/main/LICENSE")
