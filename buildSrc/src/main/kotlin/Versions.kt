0edc7e0d046fcff72e7922e37331ea9d53c47db0
Tompee <tompee@drivemode.com>
2022-06-28 17:03:15 +0900

    Update dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,10 +4,10 @@ object Versions {
     const val minSdk = 26
     const val targetSdk = 31
 
-    const val agp = "7.0.4"
-    const val kotlin = "1.6.10"
-    const val coroutines = "1.6.0"
-    const val dokka = "1.6.10"
+    const val agp = "7.2.1"
+    const val kotlin = "1.6.21"
+    const val coroutines = "1.6.2"
+    const val dokka = "1.6.21"
 
     object AndroidX {
         const val core = "1.7.0"
@@ -17,7 +17,7 @@ object Versions {
     }
 
     const val spotless = "6.1.0"
-    const val ksp = "1.6.10-1.0.2"
+    const val ksp = "1.7.0-1.0.6"
 
     object AutoService {
         const val core = "1.0"
@@ -25,7 +25,7 @@ object Versions {
     }
 
     object KotlinPoet {
-        const val core = "1.10.2"
+        const val core = "1.12.0"
         const val jvm = "0.4.0"
     }
 }
